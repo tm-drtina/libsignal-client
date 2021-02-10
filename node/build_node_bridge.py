35a62fc685b7ab10a26e4683472d8b9b18c389f4
Jordan Rose <jrose@signal.org>
2021-02-09 16:05:41 -0800

    build_node_bridge.py: fix debug build

--- node/build_node_bridge.py
+++ node/build_node_bridge.py
@@ -51,7 +51,7 @@ def main(args=None):
 
     out_dir = options.out_dir.strip('"') or os.path.join('build', configuration_name)
 
-    cmdline = ['cargo', 'build', '-p', 'libsignal-node'] + ['--release'] if configuration_name == 'Release' else []
+    cmdline = ['cargo', 'build', '-p', 'libsignal-node'] + (['--release'] if configuration_name == 'Release' else [])
     print("Running '%s'" % (' '.join(cmdline)))
 
     cargo_env = os.environ.copy()
